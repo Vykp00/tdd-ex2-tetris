20b3b09766f421f49b394010b3ce06be6c846de9
Viivi Kauppinen <hellothaovy@yahoo.com.vn>
2024-01-09 09:36:53 +0200

    Add board function

--- src/Board.mjs
+++ src/Board.mjs
@@ -6,8 +6,15 @@ export class Board {
     this.width = width;
     this.height = height;
   }
-
+  // Print board
   toString() {
-    return "TODO";
+    let shape = '';
+    for (let row = 0; row < this.height; row++) {
+      for (let col = 0; col < this.width; col++) {
+        shape += '.';
+      }
+      shape+= '\n'
+    }
+    return shape;
   }
 }
