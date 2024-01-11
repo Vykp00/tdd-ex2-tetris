4f7e960975b1b0aaad3a11d7ce18a91d5dda4e38
Viivi Kauppinen <hellothaovy@yahoo.com.vn>
2024-01-11 14:03:58 +0200

    format Board

--- src/Board.ts
+++ src/Board.ts
@@ -1,15 +1,19 @@
 import { Shape, shapeToString } from "./shapeutils";
-export class Board implements Shape{
+export class Board implements Shape {
   #width: number;
   #height: number;
   constructor(width: number, height: number) {
     this.#width = width;
     this.#height = height;
   }
-  width() {return this.#width;}
-  height() {return this.#height;}
+  width() {
+    return this.#width;
+  }
+  height() {
+    return this.#height;
+  }
   // Print board
   toString() {
-    return shapeToString(this)
+    return shapeToString(this);
   }
 }
