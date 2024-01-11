76b729018a068386f1620ad98b31ea36464b65e4
Viivi Kauppinen <hellothaovy@yahoo.com.vn>
2024-01-11 14:02:56 +0200

    Refactor 2

--- src/Board.ts
+++ src/Board.ts
@@ -1,12 +1,13 @@
 import { Shape, shapeToString } from "./shapeutils";
 export class Board implements Shape{
-  width: number;
-  height: number;
-
+  #width: number;
+  #height: number;
   constructor(width: number, height: number) {
-    this.width = width;
-    this.height = height;
+    this.#width = width;
+    this.#height = height;
   }
+  width() {return this.#width;}
+  height() {return this.#height;}
   // Print board
   toString() {
     return shapeToString(this)
--- src/shapeutils.ts
+++ src/shapeutils.ts
@@ -1,11 +1,11 @@
 export interface Shape {
-    width: number;
-    height: number
+    width(): number;
+    height(): number
 }
 export function shapeToString(inShape: Shape) {
     let outShape = '';
-    for (let row = 0; row < inShape.height; row++) {
-        for (let col = 0; col < inShape.width; col++) {
+    for (let row = 0; row < inShape.height(); row++) {
+        for (let col = 0; col < inShape.width(); col++) {
         outShape += '.';
         }
         outShape+= '\n'
