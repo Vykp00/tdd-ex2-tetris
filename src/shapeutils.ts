d41d50f41769e849bd5e8829efcfa97249800be1
Viivi Kauppinen <hellothaovy@yahoo.com.vn>
2024-01-11 14:04:35 +0200

    Reformat shapeutils

--- src/shapeutils.ts
+++ src/shapeutils.ts
@@ -1,14 +1,14 @@
 export interface Shape {
-    width(): number;
-    height(): number
+  width(): number;
+  height(): number;
 }
 export function shapeToString(inShape: Shape) {
-    let outShape = '';
-    for (let row = 0; row < inShape.height(); row++) {
-        for (let col = 0; col < inShape.width(); col++) {
-        outShape += '.';
-        }
-        outShape+= '\n'
+  let outShape = "";
+  for (let row = 0; row < inShape.height(); row++) {
+    for (let col = 0; col < inShape.width(); col++) {
+      outShape += ".";
     }
-    return outShape;
+    outShape += "\n";
+  }
+  return outShape;
 }
