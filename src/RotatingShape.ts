ded6d774d78331426e56854a230274095bccbd64
Viivi Kauppinen <hellothaovy@yahoo.com.vn>
2024-01-21 20:51:32 +0200

    pass test 1 level 2

--- src/RotatingShape.ts
+++ src/RotatingShape.ts
@@ -1,8 +1,11 @@
 export class RotatingShape {
     shape: string
-    constructor(shape: string) {this.shape = shape}
+    tetro: string
+    constructor(shape: string) {
+        this.shape = shape
+        this.tetro = this.shape.slice(0,4)+this.shape.slice(9,13)+ this.shape.slice(18,22)+'\n'
+    }
     toString(){
-        const tetro : string = this.shape.slice(0,4)+this.shape.slice(9,13)+ this.shape.slice(18,22)+'\n';
-        return tetro
+        return this.tetro
     }
 }
