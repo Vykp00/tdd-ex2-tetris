02b90209f4de7590a553ca50d2a230721c79445c
Viivi Kauppinen <hellothaovy@yahoo.com.vn>
2024-01-27 19:23:17 +0200

    refactor to implement Shape

--- src/Tetromino.ts
+++ src/Tetromino.ts
@@ -1,3 +1,31 @@
-export class Tetromino {
-    static T_SHAPE = '.T.\nTTT\n...\n'
+import {RotatingShape} from "./RotatingShape";
+import {Shape} from "./shapeutils";
+export class Tetromino implements Shape {
+    static T_SHAPE = new Tetromino(
+        0,
+        4,
+        '.T.\nTTT\n...\n'
+    );
+    #initDirection: number;
+    #directions: RotatingShape[];
+
+    constructor(
+        initDirection: number,
+        directions: number | RotatingShape[],
+        initialShape?: string
+    ) {
+        directions = directions as number;
+        this.#initDirection = initDirection;
+        const shape = new RotatingShape(initialShape);
+        this.#directions = [
+            shape,
+            shape.rotateRight(),
+        ].slice(0, directions as number);
+    }
+    #shape() {
+        return this.#directions[this.#initDirection];
+    }
+    toString() {
+        return this.#shape().toString();
+    }
 }
