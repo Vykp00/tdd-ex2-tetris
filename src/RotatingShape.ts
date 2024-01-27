47837158c355053103e57e0218921a57f62e37e6
Viivi Kauppinen <hellothaovy@yahoo.com.vn>
2024-01-27 05:34:56 +0200

    Removed hardcode. Last refactor for current test

--- src/RotatingShape.ts
+++ src/RotatingShape.ts
@@ -16,14 +16,11 @@ function convertRotateRight(shape: string[][]) {
             rotated[row][column] = shape[size - 1 - column][row];
         }
     }
-    return new RotatingShape(rotated, rotated, 0);
+    return new RotatingShape(rotated);
 }
 export class RotatingShape implements Shape {
-    #shape: string
     #shape2: string[][];
-    tetro: string
-    direction: number | null
-    constructor(shape: string, shape2: string | string[][], direction: number | null) {
+    constructor(shape2: string | string[][]) {
       if (typeof shape2 === 'string') {
         this.#shape2 = shape2
           .replaceAll(' ', '') // Remove space between letter
@@ -33,9 +30,6 @@ export class RotatingShape implements Shape {
       } else {
         this.#shape2 =shape2
       }
-        this.#shape = shape
-        this.tetro = this.#shape.slice(0,4)+this.#shape.slice(9,13)+ this.#shape.slice(18,22)+'\n'
-        this.direction = direction
     }
     width(): number {
         return this.#shape2[0].length;
@@ -48,26 +42,12 @@ export class RotatingShape implements Shape {
     }
 
     rotateRight() {
-        if (typeof this.#shape2 === 'string[][]') {
-            return convertRotateRight(this.#shape2);
-        } else {
-        const rotated = this.tetro.slice(-4,-3)+this.tetro.slice(4,5)+this.tetro.slice(0,1)+'\n'+this.tetro.slice(-3,-2)+this.tetro.slice(5,6)+this.tetro.slice(1,2)+'\n'+this.tetro.slice(-2,-1)+this.tetro.slice(6,7)+this.tetro.slice(2,3)+'\n'
-        return new RotatingShape(rotated, rotated, 1)
-        }
+        return convertRotateRight(this.#shape2);
     }
     rotateLeft() {
-        if (typeof this.#shape2 === 'string[][]') {
-            return this.rotateRight().rotateRight().rotateRight();
-        } else {
-            const rotated2 = this.tetro.slice(2, 3) + this.tetro.slice(6, 7) + this.tetro.slice(-2, -1) + '\n' + this.tetro.slice(1, 2) + this.tetro.slice(5, 6) + this.tetro.slice(-3, -2) + '\n' + this.tetro.slice(0, 1) + this.tetro.slice(4, 5) + this.tetro.slice(-4, -3) + '\n'
-            return new RotatingShape(rotated2, rotated2, 2)
-        }
+        return this.rotateRight().rotateRight().rotateRight();
     }
     toString(){
-        if (typeof this.#shape2 === 'string[][]') {
-            return shapeToString(this);
-        } else if (this.direction === 1 || this.direction === 2) {
-            return this.#shape
-        } else {return this.tetro}
+        return shapeToString(this);
     }
 }
