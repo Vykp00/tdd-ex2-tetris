e9275ef5fe2a33b09f506512c895da1b500a9c1a
Viivi Kauppinen <hellothaovy@yahoo.com.vn>
2024-05-17 21:01:45 +0300

    Refactoring at level 5: simplifying class Point

--- src/Board.ts
+++ src/Board.ts
@@ -3,13 +3,7 @@ import { Shape, shapeToString } from "./shapeutils";
 const EMPTY = ".";
 
 class Point {
-    row: number;
-    col: number;
-
-    constructor(row: number, col: number) {
-        this.row = row;
-        this.col = col;
-    }
+    constructor( public row: number, public col: number) {}
 }
 class Block implements Shape {
     #block
