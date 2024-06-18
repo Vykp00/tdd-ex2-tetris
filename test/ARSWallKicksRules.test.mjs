b03fc59557825f44de8d4acca6cad30ab55d8167
Vykp00 <vy.le@edu.karelia.fi>
2024-06-18 23:55:54 +0300

    level 7: Refactor Test. ARS Wall kick rules for L-shape

---
+++ test/ARSWallKicksRules.test.mjs
@@ -0,0 +1,33 @@
+import { afterEach, beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Tetromino, Tetromino2 } from "../src/Tetromino.ts";
+import { Board, Board2 } from "../src/Board";
+import { moveBeyondBoard } from "./MovingTetrominoes.test.mjs";
+
+describe("In ARS wall kicks,", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board2(10, 6);
+    board.drop(Tetromino2.I_SHAPE);
+    board.rotateTetro(1)
+    moveBeyondBoard(board, "left");
+    moveBeyondBoard(board, "down");
+
+  });
+  test("The I shape will never kick", () => {
+    board.drop(Tetromino2.I_SHAPE)
+    board.rotateTetro(3);
+    board.tick()
+    board.tick()
+    moveBeyondBoard(board, "left");
+    board.rotateTetro(3);
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       .I........
+       II........
+       II........
+       II........
+       I.........`);
+  });
+});
